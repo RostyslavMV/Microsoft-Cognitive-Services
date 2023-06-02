1b834411b5d93b75115d314be9671255b27a18ac
Rostyslav Mochulskyi <rostyslav.mochulskyi@acuityads.com>
2023-06-02 09:33:36 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Computer-Vision-API"
+
---
+++ src/main/java/org/example/AnalyzeImage.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.net.URI;
+
+public class AnalyzeImage {
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    private static String key = "e55ceda8507547e58c7511c437a45eac";
+    private static String endpoint = "https://computer-vision-rmv.cognitiveservices.azure.com/";
+
+    private static final String uriBase = endpoint + "vision/v3.1/analyze";
+    private static final String imageToAnalyze =
+            "https://upload.wikimedia.org/wikipedia/commons/1/12/Broadway_and_Times_Square_by_night.jpg";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            URIBuilder builder = new URIBuilder(uriBase);
+
+            // Request parameters. All of them are optional.
+            builder.setParameter("visualFeatures", "Categories,Description,Color");
+
+            // Prepare the URI for the REST API method.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Call the REST API method and get the response entity.
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("REST Response:\n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
+}
